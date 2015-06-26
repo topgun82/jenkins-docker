5c31edcf8d800a50e3ee1f20f592db3b1da91b46
David Carboni <david@carboni.io>
2015-06-26 12:07:25 +0100

    Updated the build script with details of the AWS environment.

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-docker build --tag=localhost:5000/jenkins .
-docker push localhost:5000/jenkins
+docker build --no-cache --tag=onsdigital/jenkins .
+docker tag -f onsdigital/jenkins carboni.io/jenkins
+docker push carboni.io/jenkins
